9ac02a113fe5fdbe75b5735812c950b098a43264
Ferenczy Csilla <csilla.ferenczy@basewalk.com>
2017-09-11 17:06:56 +0200

    search

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -15,6 +15,8 @@ export class AlbumsComponent implements OnInit {
   nextToken: string;
   nodeId: string;
   nodeName: string;
+  searchValue: string;
+  searchMode = false;
 
   ngOnInit() {
     this.nodeId = this.route.snapshot.paramMap.get('nodeId');
@@ -36,7 +38,7 @@ export class AlbumsComponent implements OnInit {
 
   onScroll() {
     console.log('scrolled!!')
-    if (this.nextToken) {
+    if (!this.searchMode && this.nextToken) {
       this.service.getNextAlbumsSortedByUpdateTimeDesc(this.nextToken).then(
         (res: any) => this.setAlbums(res)
       );
@@ -55,5 +57,24 @@ export class AlbumsComponent implements OnInit {
         this.nextToken = null;
       }
     }
+  }
+
+  searchAlbums(searchValue) {
+    this.searchValue = searchValue;
+    this.searchMode = true;
+    this.albums = null;
+    this.service.getAllAlbumsSortedByCreationDateDesc(this.nodeId).then(
+      (res: any) => this.albums = res.filter(this.filterBySearchValue.bind(null, searchValue))
+    );
+  }
+
+  filterBySearchValue(searchValue, album) {
+    return album.name && album.name.search(searchValue) > -1;
+  }
+
+  showAll() {
+    this.searchMode = false;
+    this.albums = null;
+    this.init();
   }
 }
